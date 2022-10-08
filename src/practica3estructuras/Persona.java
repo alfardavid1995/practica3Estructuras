1f5c6c62d48812a3dc1e700ed1339dea450908fe
AleMendezS <98509409+AleMendezS@users.noreply.github.com>
2022-10-08 15:32:35 -0600

    creacion de variables, get y set en la clase persona

--- src/practica3estructuras/Persona.java
+++ src/practica3estructuras/Persona.java
@@ -11,6 +11,22 @@ public class Persona {
         this.nombre = nombre;
         this.cedula = cedula;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
     
     
     
