2d0e7ecc60102264d06d3faa860df6c01a006ba5
marianaalvarezvillalta <marianaalv07@gmail.com>
2022-10-08 15:22:54 -0600

    tarea colas(nodo)

---
+++ src/practica3estructuras/Nodo.java
@@ -0,0 +1,29 @@
+package practica3estructuras;
+
+public class Nodo {
+    private String dato;
+    private Nodo atras;
+
+    public String getDato() {
+        return dato;
+    }
+
+    public void setDato(String dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getAtras() {
+        return atras;
+    }
+
+    public void setAtras(Nodo atras) {
+        this.atras = atras;
+    }
+
+    public Nodo(String dato) {
+        this.dato = dato;
+    }
+    
+    @Override
+    public String toString(){ return dato; }
+}
