cfe45f5976a86cafa43e6c06908123c434cc35c4
AleMendezS <98509409+AleMendezS@users.noreply.github.com>
2022-10-08 15:26:20 -0600

    creacion de los get y set en la clase persona

--- src/practica3estructuras/Persona.java
+++ src/practica3estructuras/Persona.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package practica3estructuras;
 
-/**
- *
- * @author css124646
- */
+
 public class Persona {
+    private String nombre;
+    private int cedula;
+
+    public Persona(String nombre, int cedula) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+    
     
 }
