e329605db515230366ddb89641fc2515b1655781
marianaalvarezvillalta <marianaalv07@gmail.com>
2022-10-08 15:36:59 -0600

    colas

--- src/practica3estructuras/Cola.java
+++ src/practica3estructuras/Cola.java
@@ -26,6 +26,28 @@ public class Cola {
         return aux;
     }
     
+    //SEARCH DE PILAS
+//    public boolean search(int reference) {
+//        // Crea una copia de la pila.
+//        Node aux = cima;
+//        // Bandera para verificar si exist el elemento a search.
+//        boolean exist = false;
+//        // Recorre la pila hasta llegar encontrar el node o llegar al final
+//        // de la pila.
+//        while (exist != true && aux != null) {
+//            // Compara si el value del node es igual que al de reference.
+//            if (reference == aux.getValor()) {
+//                // Cambia el value de la bandera.
+//                exist = true;
+//            } else {
+//                // Avanza al siguiente node.
+//                aux = aux.getSiguiente();
+//            }
+//        }
+//        // Retorna el value de la bandera.
+//        return exist;
+//    }
+    
     @Override
     public String toString(){
         String s="";
